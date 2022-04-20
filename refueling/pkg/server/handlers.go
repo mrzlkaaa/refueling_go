41b1dcfe0ec5ba305177ee0e9d4e4b5f8231acb3
Nikita <nvs38@tpu.ru>
2022-04-20 12:56:49 +0700

    added authorization check middleware, personal access ckeck

--- refueling/pkg/server/handlers.go
+++ refueling/pkg/server/handlers.go
@@ -1,17 +1,26 @@
 package server
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
 	"refueling/refueling/pkg/adding"
 	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 )
 
 const (
 	statusOkAdd    = "Data recorded"
 	statusOkDelete = "Data deleted"
+	notSerialized  = "given JSON is not serializable"
+	unauthorized   = "unauthorized"
+	noRigths       = "you are not eligible for this service"
+	expired        = "token is expired"
+	unexpected     = "something unexpected happened"
 )
 
 type Response struct {
@@ -21,20 +30,134 @@ type Response struct {
 
 func (s *Server) Router() *gin.Engine {
 	router := s.engine
-	router.GET("/refuelingsList", s.RefNames())
-	router.GET("/refuelings", s.Refuels())
-	router.GET("/refuelings/:id/details", s.RefuelDetails())
-	router.GET("/refuelings/:id/:actId/PDC", s.RefuelPDC())
-	router.GET("/refuelings/:id/:actId/download", s.Download())
-	router.POST("/add", s.Add())
-	router.POST("/add-act", s.AddAct())
-	router.POST("/refuelings/:id/:actId/delete", s.DeleteAct())
-	router.POST("/refuelings/:id/delete", s.Delete())
+	// router.GET("/refuelings", s.Refuels())
+	auth := router.Group("/")
+	auth.Use(s.AuthRequired())
+	{
+		auth.GET("/refuelingsList", s.RefNames())
+		auth.GET("/refuelings", s.Refuels())
+		auth.GET("/refuelings/:id/details", s.RefuelDetails())
+		auth.GET("/refuelings/:id/:actId/PDC", s.RefuelPDC())
+		auth.GET("/refuelings/:id/:actId/download", s.Download())
+		auth.POST("/add", s.Add())
+		auth.POST("/add-act", s.AddAct())
+		auth.POST("/refuelings/:id/:actId/delete", s.DeleteAct())
+		auth.POST("/refuelings/:id/delete", s.Delete())
+	}
+
 	return router
+}
+
+func (s *Server) AuthRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		claims, err := ValidateToken(c)
+		if err != nil {
+			// c.JSON(http.StatusUnauthorized, err.Error())
+			c.AbortWithError(http.StatusUnauthorized, err)
+		}
+		rights, err := s.FetchValue(claims["access_uuid"].(string))
+		if err != nil {
+			c.AbortWithError(http.StatusUnauthorized, err)
+		}
+		moderator, admin, err := ParseRights(rights)
+		if err != nil {
+			c.AbortWithError(http.StatusUnauthorized, err)
+		}
+		c.Set("access_uuid", claims["access_uuid"])
+		c.Set("moderator", moderator)
+		c.Set("admin", admin)
+		c.Next()
+	}
+}
+
+func FetchAuth(c *gin.Context) (map[string]interface{}, error) {
+	claims, err := ValidateToken(c)
+	if err != nil {
+		// c.JSON(http.StatusUnauthorized, err)
+		return map[string]interface{}{}, err
+	}
+
+	return claims, nil
+}
+
+func ValidateToken(c *gin.Context) (map[string]interface{}, error) {
+	auth := c.Request.Header.Get("Authorization")
+	if auth == "" {
+		return map[string]interface{}{}, errors.New("bearer token is not given")
+	}
+	tokenString := strings.Split(auth, " ")[len(strings.Split(auth, " "))-1]
+	token, err := VerifyToken(tokenString)
+	fmt.Println(token, err)
+	if err != nil {
+		return map[string]interface{}{}, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !token.Valid && !ok {
+		return map[string]interface{}{}, errors.New("is not valid")
+	}
+	// uuid := claims["access_uuid"].(string)
+
+	return claims, nil
+}
+
+func VerifyToken(tokenString string) (*jwt.Token, error) { //todo add extra argument to seperate refresh and access verifications
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("ACCESS_SECRET")), nil
+
+	})
+	if err != nil {
+		ve, ok := err.(*jwt.ValidationError)
+		if ok && ve.Errors&jwt.ValidationErrorMalformed != 0 {
+			return token, errors.New("signature is invalid")
+		} else if ok && ve.Errors&(jwt.ValidationErrorExpired|jwt.ValidationErrorNotValidYet) != 0 {
+			return token, errors.New(expired)
+		} else {
+			return token, errors.New(unexpected)
+		}
+	}
+	return token, nil
+}
+
+func (s *Server) FetchValue(key string) (string, error) {
+	v, err := s.client.Get(key).Result()
+	return v, err
+}
+
+func ParseRights(rights string) (bool, bool, error) {
+	sliceRights := strings.Split(rights, ",")
+	moderatorStr, adminStr := sliceRights[0], sliceRights[1]
+	moderator, err := strconv.ParseBool(moderatorStr)
+	if err != nil {
+		return false, false, err
+	}
+	admin, _ := strconv.ParseBool(adminStr)
+	if err != nil {
+		return false, false, err
+	}
+
+	return moderator, admin, nil
 }
 
 func (s *Server) RefNames() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		moderator, ok := c.Get("moderator")
+		if !ok {
+			c.JSON(http.StatusUnauthorized, noRigths)
+		}
+
+		admin, ok := c.Get("admin")
+		if !ok {
+			c.JSON(http.StatusUnauthorized, noRigths)
+		}
+		if !moderator.(bool) && !admin.(bool) {
+			c.JSON(http.StatusUnauthorized, noRigths)
+			return
+		}
+
 		data := s.listing.GetRefuelNames()
 		c.IndentedJSON(http.StatusOK, data)
 	}
@@ -42,6 +165,20 @@ func (s *Server) RefNames() gin.HandlerFunc {
 
 func (s *Server) Refuels() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		moderator, ok := c.Get("moderator")
+		if !ok {
+			c.JSON(http.StatusUnauthorized, noRigths)
+		}
+
+		admin, ok := c.Get("admin")
+		if !ok {
+			c.JSON(http.StatusUnauthorized, noRigths)
+		}
+		if !moderator.(bool) && !admin.(bool) {
+			c.JSON(http.StatusUnauthorized, noRigths)
+			return
+		}
+
 		data := s.listing.Refuels()
 		c.IndentedJSON(http.StatusOK, data)
 	}
@@ -49,6 +186,20 @@ func (s *Server) Refuels() gin.HandlerFunc {
 
 func (s *Server) RefuelDetails() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		moderator, ok := c.Get("moderator")
+		if !ok {
+			c.JSON(http.StatusUnauthorized, noRigths)
+		}
+
+		admin, ok := c.Get("admin")
+		if !ok {
+			c.JSON(http.StatusUnauthorized, noRigths)
+		}
+		if !moderator.(bool) && !admin.(bool) {
+			c.JSON(http.StatusUnauthorized, noRigths)
+			return
+		}
+
 		idstr := c.Param("id")
 		id, _ := strconv.Atoi(idstr)
 		data := s.listing.RefuelDetails(id)
@@ -58,6 +209,20 @@ func (s *Server) RefuelDetails() gin.HandlerFunc {
 
 func (s *Server) RefuelPDC() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		moderator, ok := c.Get("moderator")
+		if !ok {
+			c.JSON(http.StatusUnauthorized, noRigths)
+		}
+
+		admin, ok := c.Get("admin")
+		if !ok {
+			c.JSON(http.StatusUnauthorized, noRigths)
+		}
+		if !moderator.(bool) && !admin.(bool) {
+			c.JSON(http.StatusUnauthorized, noRigths)
+			return
+		}
+
 		idParam := c.Param("actId")
 		id, err := strconv.Atoi(idParam)
 		if err != nil {
@@ -70,6 +235,15 @@ func (s *Server) RefuelPDC() gin.HandlerFunc {
 
 func (s *Server) Add() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		admin, ok := c.Get("admin")
+		if !ok {
+			c.JSON(http.StatusUnauthorized, noRigths)
+		}
+		if !admin.(bool) {
+			c.JSON(http.StatusUnauthorized, noRigths)
+			return
+		}
+
 		var refuel adding.Refuel
 		// var refuel interface{}
 		if err := c.BindJSON(&refuel); err != nil {
@@ -86,6 +260,15 @@ func (s *Server) Add() gin.HandlerFunc {
 
 func (s *Server) AddAct() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		admin, ok := c.Get("admin")
+		if !ok {
+			c.JSON(http.StatusUnauthorized, noRigths)
+		}
+		if !admin.(bool) {
+			c.JSON(http.StatusUnauthorized, noRigths)
+			return
+		}
+
 		var act adding.Act
 		// var refuel interface{}
 		if err := c.BindJSON(&act); err != nil {
@@ -104,6 +287,15 @@ func (s *Server) AddAct() gin.HandlerFunc {
 
 func (s *Server) Delete() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		admin, ok := c.Get("admin")
+		if !ok {
+			c.JSON(http.StatusUnauthorized, noRigths)
+		}
+		if !admin.(bool) {
+			c.JSON(http.StatusUnauthorized, noRigths)
+			return
+		}
+
 		// var refuel interface{}
 		idParam := c.Param("id")
 		id, err := strconv.Atoi(idParam)
@@ -123,6 +315,15 @@ func (s *Server) Delete() gin.HandlerFunc {
 
 func (s *Server) DeleteAct() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		admin, ok := c.Get("admin")
+		if !ok {
+			c.JSON(http.StatusUnauthorized, noRigths)
+		}
+		if !admin.(bool) {
+			c.JSON(http.StatusUnauthorized, noRigths)
+			return
+		}
+
 		// var refuel interface{}
 		idParam := c.Param("actId")
 		id, err := strconv.Atoi(idParam)
@@ -142,6 +343,20 @@ func (s *Server) DeleteAct() gin.HandlerFunc {
 
 func (s *Server) Download() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		moderator, ok := c.Get("moderator")
+		if !ok {
+			c.JSON(http.StatusUnauthorized, noRigths)
+		}
+
+		admin, ok := c.Get("admin")
+		if !ok {
+			c.JSON(http.StatusUnauthorized, noRigths)
+		}
+		if !moderator.(bool) && !admin.(bool) {
+			c.JSON(http.StatusUnauthorized, noRigths)
+			return
+		}
+
 		idParam := c.Param("id")
 		id, err := strconv.Atoi(idParam)
 		if err != nil {
--- refueling/pkg/server/server.go
+++ refueling/pkg/server/server.go
@@ -1,11 +1,14 @@
 package server
 
 import (
+	"fmt"
+	"os"
 	"refueling/refueling/pkg/adding"
 	"refueling/refueling/pkg/download"
 	"refueling/refueling/pkg/listing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
 )
 
 type Server struct {
@@ -13,12 +16,15 @@ type Server struct {
 	listing  listing.ListingService
 	adding   adding.AddingService
 	download download.DownloadService
+	client   *redis.Client
 	// refs to listing, adding and so on
 }
 
 func NewServer(engine *gin.Engine, adding adding.AddingService,
 	listing listing.ListingService, download download.DownloadService) *Server {
-	return &Server{engine: engine, adding: adding, listing: listing, download: download}
+	client := redis.NewClient(&redis.Options{
+		Addr: fmt.Sprintf("%v:%v", os.Getenv("HOST"), "49153")})
+	return &Server{engine: engine, adding: adding, listing: listing, download: download, client: client}
 }
 
 func (s *Server) Run() {
